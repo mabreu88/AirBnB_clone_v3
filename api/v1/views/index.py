433631eaa8728ba2413c11abcd32737886c345a2
Martin <martymalkav7@gmail.com>
2023-12-23 14:23:56 -0800

    add dict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,21 @@ def statusRoute():
     """Status Route"""
     if request.method == 'GET':
         return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def count():
+    """ Count objects """
+    dictob = {}
+    clss = {
+        "Amenity": "amenities",
+        "Place": "places",
+        "State": "states",
+        "City": "cities",
+        "Review": "reviews",
+        "User": "users"
+    }
+
+    for cls in clss:
+        count = storage.count(cls)
+        dictob[clss.get(cls)] = count
+    return jsonify(dictob)
